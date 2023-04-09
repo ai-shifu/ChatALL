846c382ebc2cabf78351b7343fd0392546e60dd2
Sunner Sun <sunner@gmail.com>
2023-04-09 14:18:56 +0800

    Change framework to Vue by GPT-4

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ChatALL
 Chat with all AI bots, discover the best
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+      <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>ChatALL</h1>
+    <h2>{{ slogan }}</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            slogan: 'Chat all AI bots, get the best',
+        };
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
