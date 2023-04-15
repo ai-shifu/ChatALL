98e0f8d0d12cc47d3749ff2654755f5422160fe0
Sunner Sun <sunner@gmail.com>
2023-04-15 12:09:28 +0800

    Refactor i18n file structure

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,50 +1,12 @@
 import { createI18n } from "vue-i18n";
 
+// Import translation files here
+import enMessages from "./locales/en.json";
+import zhMessages from "./locales/zh.json";
+
 const messages = {
-  en: {
-    settings: "Settings",
-    general: "General",
-    save: "Save",
-    header: {
-      singleColumn: "Single Column",
-      doubleColumn: "Double Column",
-      tripleColumn: "Triple Column",
-    },
-    footer: {
-      sendPrompt: "Send",
-    },
-    bot: {
-      nullBot: "Null Bot",
-      ChatGPT: "ChatGPT",
-      BingChat: "Bing Chat",
-      Bard: "Bard",
-      ERNIE: "ERNIE",
-    },
-    modal: {
-      loginPrompt:
-        "Click the link below, log in to { botName } in the new window, and then close the window.",
-    },
-  },
-  zh: {
-    settings: "设置",
-    general: "通用",
-    save: "保存",
-    header: {
-      singleColumn: "单列",
-      doubleColumn: "双列",
-      tripleColumn: "三列",
-    },
-    footer: {
-      sendPrompt: "发送",
-    },
-    bot: {
-      nullBot: "虚无",
-      ERNIE: "文心一言",
-    },
-    modal: {
-      loginPrompt: "点击下面链接，在新窗口内登录{ botName }，然后关闭窗口",
-    },
-  },
+  en: enMessages,
+  zh: zhMessages,
 };
 
 const i18n = createI18n({
