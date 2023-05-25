38430080087544b63642cb447f1b504cefb02420
Wenjing Liu <wenj2016@gmail.com>
2023-05-25 01:25:50 +0100

    Update src/bots/openai/ChatGPT4BotMobile.js
    Co-authored-by: Sun Zhigang <sunner@gmail.com>

--- src/bots/openai/ChatGPT4BotMobile.js
+++ src/bots/openai/ChatGPT4BotMobile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import ChatGPTBot from "./ChatGPTBot";
 
 export default class ChatGPT4BotMobile extends ChatGPTBot {
-  static _className = "ChatGPT4Bot-mobile"; // Class name of the bot
+  static _className = "ChatGPT4MobileBot"; // Class name of the bot
   static _logoFilename = "chatgpt-4-mobile-logo.png"; // Place it in assets/bots/
   static _model = "gpt-4-mobile";
 
