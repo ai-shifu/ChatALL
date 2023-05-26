86f0e502e4cda626231bcabdec5ad9b22356e55a
Lucas Robin <lucas.rob1@live.fr>
2023-05-27 00:55:09 +0200

    feat: create index for bots

---
+++ src/bots/index.js
@@ -0,0 +1,41 @@
+import ChatGPT35Bot from "./openai/ChatGPT35Bot";
+import ChatGPT4Bot from "./openai/ChatGPT4Bot";
+import ChatGPTBrowsingBot from "./openai/ChatGPTBrowsingBot";
+import BingChatPreciseBot from "./microsoft/BingChatPreciseBot";
+import BingChatBalancedBot from "./microsoft/BingChatBalancedBot";
+import BingChatCreativeBot from "./microsoft/BingChatCreativeBot";
+import SparkBot from "./SparkBot";
+import BardBot from "./BardBot";
+import OpenAIAPI35Bot from "./openai/OpenAIAPI35Bot";
+import OpenAIAPI4Bot from "./openai/OpenAIAPI4Bot";
+import MOSSBot from "./MOSSBot";
+import WenxinQianfanBot from "./baidu/WenxinQianfanBot";
+import VicunaBot from "./lmsys/VicunaBot";
+import ChatGLMBot from "./lmsys/ChatGLMBot";
+import AlpacaBot from "./lmsys/AlpacaBot";
+import ClaudeBot from "./lmsys/ClaudeBot";
+import GradioAppBot from "./huggingface/GradioAppBot";
+import HuggingChatBot from "./huggingface/HuggingChatBot";
+import DevBot from "./DevBot";
+
+export default {
+  ChatGPT35Bot,
+  ChatGPT4Bot,
+  ChatGPTBrowsingBot,
+  BingChatPreciseBot,
+  BingChatBalancedBot,
+  BingChatCreativeBot,
+  SparkBot,
+  BardBot,
+  OpenAIAPI35Bot,
+  OpenAIAPI4Bot,
+  MOSSBot,
+  WenxinQianfanBot,
+  VicunaBot,
+  ChatGLMBot,
+  AlpacaBot,
+  ClaudeBot,
+  GradioAppBot,
+  HuggingChatBot,
+  DevBot,
+}
