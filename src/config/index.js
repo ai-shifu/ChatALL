82d81721ba4eb1de35fff2123bbea36617f6203f
Lucas Robin <lucas.rob1@live.fr>
2023-05-27 00:55:31 +0200

    feat: create config folder and file

---
+++ src/config/index.js
@@ -0,0 +1,25 @@
+export const AVAILABLE_BOT_NAMES = [
+  'ChatGPT35Bot',
+  'ChatGPT4Bot',
+  'ChatGPTBrowsingBot',
+  'BingChatPreciseBot',
+  'BingChatBalancedBot',
+  'BingChatCreativeBot',
+  'SparkBot',
+  'BardBot',
+  'OpenAIAPI35Bot',
+  'OpenAIAPI4Bot',
+  'MOSSBot',
+  'WenxinQianfanBot',
+  'VicunaBot',
+  'ChatGLMBot',
+  'AlpacaBot',
+  'ClaudeBot',
+  'GradioAppBot',
+  'HuggingChatBot',
+  'DevBot',
+]
+
+export default {
+  AVAILABLE_BOT_NAMES,
+}
