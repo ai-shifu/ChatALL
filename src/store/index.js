d4f9110c965443139c206a44aaf70aef0ef78ad2
Lucas Robin <lucas.rob1@live.fr>
2023-05-27 01:00:24 +0200

    feat: persist settings module

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-// import VuexPersistence from "vuex-persist";
+import VuexPersistence from "vuex-persist";
 import { createStore } from "vuex";
 
 import appModule from './app.module'
 import chatsModule from './chats.module'
 import settingsModule from './settings.module'
 
-// const persistedStore = new VuexPersistence({
-//   key: "chatall-data",
-//   storage: window.localStorage,
-//   modules: ["chatsModule", "appModule"],
-// });
+const persistedStore = new VuexPersistence({
+  key: "chatall-data",
+  storage: window.localStorage,
+  modules: [
+    "chatsModule", 
+    "appModule", 
+    "settingsModule"
+  ],
+});
 
 const store = createStore({
   modules: {
@@ -17,9 +21,9 @@ const store = createStore({
     chatsModule,
     settingsModule,
   },
-  // plugins: process.env.NODE_ENV === 'production'
-  //   ? [persistedStore.plugin]
-  //   : []
+  plugins: process.env.NODE_ENV === 'production'
+    ? [persistedStore.plugin]
+    : []
 })
 
 export default store
