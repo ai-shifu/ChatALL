969736a857eb6c1af54e40a923c11181532114a4
Youth <qcgm197874@gmail.com>
2023-07-04 15:06:55 +0800

    Set the "filter" CSS property for dark-colored icons under a dark the… (#340)
    * Set the "filter" CSS property for dark-colored icons under a dark theme to "invert(100%)"
    
    * chore: fix style
    
    * chore: move const into function
    
    ---------
    
    Co-authored-by: Sunner Sun <sunner@gmail.com>

--- src/components/Footer/BotLogo.vue
+++ src/components/Footer/BotLogo.vue
@@ -1,7 +1,7 @@
 <template>
   <v-avatar
     rounded="rounded"
-    :class="{ active: active }"
+    :class="{ active: active, filter: enable_filter(bot) }"
     :image="bot.getLogo()"
     :alt="bot.getFullname()"
     :title="bot.getFullname()"
@@ -9,7 +9,26 @@
 </template>
 
 <script setup>
+import { useStore } from "vuex";
+import { Theme } from "@/theme";
+
+const store = useStore();
+
 defineProps(["bot", "active"]);
+
+function enable_filter(bot) {
+  const dark_icon_classname = [
+    "OpenAIAPI35Bot",
+    "OpenAIAPI3516KBot",
+    "OpenAIAPI4Bot",
+    "SkyWorkBot",
+    "AzureOpenAIAPIBot",
+    "DevBot",
+  ];
+  const is_dark = store.state.theme == Theme.DARK;
+  const is_dark_bot = dark_icon_classname.includes(bot.getClassname());
+  return is_dark && is_dark_bot;
+}
 </script>
 
 <style>
@@ -22,5 +41,9 @@ defineProps(["bot", "active"]);
 .active {
   opacity: 1;
   filter: grayscale(0%);
+}
+
+.filter{
+  filter: invert(100%);
 }
 </style>
