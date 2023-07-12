9444dbc2eea03cd1887cb04fc1b35ce4f03ee44e
qcgm197874@gmail.com <qcgm197874@gmail.com>
2023-07-12 20:23:00 +0800

    identity OpenAIAPIBot 429 error(exceeded current quota)  when activing bot

--- src/bots/openai/OpenAIAPIBot.js
+++ src/bots/openai/OpenAIAPIBot.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import LangChainBot from "@/bots/LangChainBot";
 import store from "@/store";
 import { ChatOpenAI } from "langchain/chat_models/openai";
@@ -14,17 +15,32 @@ export default class OpenAIAPIBot extends LangChainBot {
     if (!store.state.openaiApi.apiKey) {
       this.constructor._isAvailable = false;
     } else {
-      const chatModel = new ChatOpenAI({
-        configuration: {
-          basePath: store.state.openaiApi.alterUrl ? store.state.openaiApi.alterUrl : "",
+      const subscription = await axios.get(
+        "https://api.openai.com/v1/dashboard/billing/subscription",
+        {
+          headers: {
+            Authorization: `Bearer ${store.state.openaiApi.apiKey}`,
+          },
         },
-        openAIApiKey: store.state.openaiApi.apiKey,
-        modelName: this.constructor._model ? this.constructor._model : "",
-        temperature: store.state.openaiApi.temperature,
-        streaming: true,
-      });
-      this.constructor._chatModel = chatModel;
-      this.constructor._isAvailable = true;
+      );
+      if (subscription.data.access_until * 1000 < new Date().getTime()) {
+        console.log(
+          "You exceeded your current quota, please check your plan and billing details.",
+        );
+        this.constructor._isAvailable = false;
+      } else {
+        const chatModel = new ChatOpenAI({
+          configuration: {
+          basePath: store.state.openaiApi.alterUrl ? store.state.openaiApi.alterUrl : "",
+          },
+          openAIApiKey: store.state.openaiApi.apiKey,
+          modelName: this.constructor._model ? this.constructor._model : "",
+          temperature: store.state.openaiApi.temperature,
+          streaming: true,
+        });
+        this.constructor._chatModel = chatModel;
+        this.constructor._isAvailable = true;
+      }
     }
     return this.isAvailable();
   }
