a6cec861659fbe0cf29db1a139154eec67af45e2
qcgm197874@gmail.com <qcgm197874@gmail.com>
2023-07-15 11:27:06 +0800

    add button to Download All Chat History in dialog panel of setting page.

--- src/components/ChatSetting.vue
+++ src/components/ChatSetting.vue
@@ -6,6 +6,13 @@
       :text="$t('chat.deleteAllChatHistory')"
       @click="deleteChats"
     ></v-btn>
+    <v-btn
+      color="primary"
+      variant="outlined"
+      :text="$t('chat.downloadAllChatHistory')"
+      @click="downloadJson"
+      style="margin-left: 10px"
+    ></v-btn>
   </v-list-item>
   <ConfirmModal ref="confirmModal" />
 </template>
@@ -18,6 +25,24 @@ import ConfirmModal from "@/components/ConfirmModal.vue";
 const emit = defineEmits(["close-dialog"]);
 const confirmModal = ref();
 const store = useStore();
+
+const downloadJson = () => {
+  const to_save = JSON.parse(localStorage["chatall-messages"]).chats.map((d) =>
+    d.messages.map((t) => ({ type: t.type, content: t.content })),
+  );
+  const blob = new Blob([JSON.stringify(to_save)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  const file_name = `chatall-history-${new Date()
+    .toLocaleDateString()
+    .replace(/\//g, "")}`;
+  a.download = `${file_name}.json`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+};
 async function deleteChats() {
   const confirm = await confirmModal.value.showModal(
     "",
