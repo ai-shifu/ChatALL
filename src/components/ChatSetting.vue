4b66932188e191d5d0583fb5d13ad2e7cbecc7b2
tanchekwei <weiwei1@live.com.my>
2023-08-12 19:43:08 +0800

    Add chat action management

--- src/components/ChatSetting.vue
+++ src/components/ChatSetting.vue
@@ -16,21 +16,177 @@
       ></v-btn>
     </v-list-item>
     <v-list-item>
-      <v-list-item-title>{{ $t("chat.actions") }} </v-list-item-title>
+      <v-row>
+        <v-col class="align-baseline d-flex" style="font-size: 1.5rem">
+          <label class="pr-3">{{ $t("chat.actions") }}</label>
+          <v-btn
+            class="mt-1"
+            color="primary"
+            variant="outlined"
+            prepend-icon="mdi-plus"
+            :text="$t('chat.addAction')"
+            @click="add"
+          ></v-btn>
+        </v-col>
+      </v-row>
+    </v-list-item>
+    <v-list-item
+      v-for="action in userActions"
+      density="comfortable"
+      :key="action.index"
+      :value="action.index"
+      :ripple="false"
+    >
+      <div class="align-center d-flex">
+        <v-list-item-title class="pr-5">{{ action.name }}</v-list-item-title>
+        <v-btn
+          flat
+          size="x-small"
+          icon="mdi-pencil-outline"
+          @click="edit(action)"
+          style="background-color: transparent"
+        ></v-btn>
+        <v-btn
+          flat
+          size="x-small"
+          icon="mdi-delete-outline"
+          @click="deleteAction(action)"
+          style="margin: 0; background-color: transparent"
+        ></v-btn>
+      </div>
     </v-list-item>
   </v-list>
+  <v-dialog
+    persistent
+    width="90%"
+    height="90%"
+    :model-value="isOpenAddEditAction"
+    @update:model-value="isOpenAddEditAction = $event"
+  >
+    <v-card>
+      <v-form ref="formRef" class="pa-3" @submit.prevent>
+        <v-text-field
+          required
+          autofocus
+          v-model="actionName"
+          placeholder="Summarize"
+          :label="$t('chat.actionName')"
+          :rules="[validationRules.required]"
+        ></v-text-field>
+        <v-textarea
+          required
+          rows="10"
+          v-model="template"
+          :placeholder="templatePlaceholder"
+          :label="$t('chat.actionTemplate')"
+          :rules="[validationRules.required, validationRules.template]"
+          @input="onInputTemplate"
+        >
+          <template v-slot:append-inner>
+            <v-btn
+              flat
+              v-bind="props"
+              size="x-small"
+              icon="mdi-help"
+              style="background-color: inherit"
+              @click="isShowTemplateGuideDialog = !isShowTemplateGuideTooltip"
+            >
+            </v-btn>
+          </template>
+        </v-textarea>
+        <label class="pl-4" style="font-size: 1.2rem">{{
+          $t("chat.preview")
+        }}</label>
+        <v-md-preview class="pa-4" :text="previewRef" />
+      </v-form>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          variant="outlined"
+          color="primary"
+          @click="isOpenAddEditAction = false"
+          >{{ $t("modal.cancel") }}</v-btn
+        >
+        <v-btn variant="flat" class="bg-primary" @click="addEditAction">{{
+          $t("modal.done")
+        }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <v-dialog
+    width="auto"
+    :model-value="isShowTemplateGuideDialog"
+    v-on:after-leave="isShowTemplateGuideDialog = false"
+  >
+    <v-card>
+      <v-md-preview class="pa-4" :text="$t('chat.actionTemplateGuide')" />
+    </v-card>
+  </v-dialog>
   <ConfirmModal ref="confirmModal" />
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex";
 import i18n from "@/i18n";
 import ConfirmModal from "@/components/ConfirmModal.vue";
 import bots from "@/bots";
+import {
+  preview,
+  templateKey,
+  templatePlaceholder,
+} from "../helpers/template-parser";
 const emit = defineEmits(["close-dialog"]);
 const confirmModal = ref();
+const formRef = ref(null);
+const isOpenAddEditAction = ref(false);
+const actionName = ref("");
+const template = ref("");
+const previewRef = ref("");
+const isShowTemplateGuideDialog = ref(false);
 const store = useStore();
+const userActions = computed(() => {
+  return store.state.actions.filter((p) => !p.hide);
+});
+const templateKeyRegex = {
+  loopStart: new RegExp(templateKey.loopStart, "gm"),
+  loopEnd: new RegExp(templateKey.loopEnd, "gm"),
+  botName: new RegExp(templateKey.botName, "gm"),
+  botResponse: new RegExp(templateKey.botResponse, "gm"),
+};
+const previewSampleData = [
+  {
+    botName: "Bing Chat (Creative)",
+    botResponse: "Hello, this is Bing. How are you today? 😊",
+  },
+  {
+    botName: "ChatGPT (GPT-3.5)",
+    botResponse: "Hello! How can I assist you today?",
+  },
+  {
+    botName: "Bard",
+    botResponse: "Hi there! How can I help you today?",
+  },
+  {
+    botName: "YouChat",
+    botResponse: "Hi there! How can I assist you today?",
+  },
+];
+let editIndex = undefined;
+let isEdit = false;
+const validationRules = {
+  required: (value) =>
+    value?.trim() ? true : i18n.global.t("prompt.required"),
+  template: (value) => {
+    let message = "";
+    for (const key in templateKey) {
+      if (!value.match(templateKeyRegex[key])) {
+        message += i18n.global.t(`chat.${key}Required`) + ", ";
+      }
+    }
+    return message ? message.substring(0, message.length - 2) : true;
+  },
+};
 
 // This function downloads the chat history as a JSON file.
 const downloadJson = () => {
@@ -119,4 +275,57 @@ async function deleteChats() {
     emit("close-dialog");
   }
 }
+
+function add() {
+  isEdit = false;
+  actionName.value = "";
+  template.value = templatePlaceholder;
+  isOpenAddEditAction.value = true;
+  onInputTemplate();
+}
+
+function edit(item) {
+  isEdit = true;
+  actionName.value = item.name;
+  template.value = item.template;
+  editIndex = item.index;
+  isOpenAddEditAction.value = true;
+  onInputTemplate();
+}
+
+async function onInputTemplate() {
+  previewRef.value = preview(template.value, previewSampleData);
+}
+
+async function addEditAction() {
+  if ((await formRef.value.validate()).valid) {
+    if (isEdit) {
+      store.commit("editAction", {
+        name: actionName.value,
+        template: template.value,
+        index: editIndex,
+      });
+    } else {
+      store.commit("addAction", {
+        name: actionName.value,
+        template: template.value,
+      });
+    }
+    isOpenAddEditAction.value = false;
+  }
+}
+
+async function deleteAction(item) {
+  const result = await confirmModal.value.showModal(
+    i18n.global.t("modal.confirmHideAction"),
+  );
+  if (result) {
+    store.commit("deleteAction", { ...item });
+  }
+}
 </script>
+<style scoped>
+.no-text-transform {
+  text-transform: none !important;
+}
+</style>
---
+++ src/helpers/template-parser.js
@@ -0,0 +1,134 @@
+const templateKey = {
+  loopStart: "{loopStart}",
+  loopEnd: "{loopEnd}",
+  botName: "{botName}",
+  botResponse: "{botResponse}",
+};
+
+const templateLoopKey = {
+  loopStart: templateKey.loopStart,
+  loopEnd: templateKey.loopEnd,
+};
+
+const templatePlaceholder = `I want you to summarize data below in a markdown table format with bot name column and difference column only. Simplify and identify the difference.
+Below is the data, each data delimited by \`details\` tag and bot name is delimited by \`summary\` tag.
+{loopStart}
+<details>
+  <summary>{botName}</summary>
+{botResponse}
+</details>
+{loopEnd}
+`;
+
+function lexer(template) {
+  let tokens = [];
+  let current = 0;
+  while (current < template.length) {
+    let char = template[current];
+    // Check for keywords
+    if (char === "{") {
+      let isMatchKeyword;
+      for (const key in templateLoopKey) {
+        if (
+          template.slice(current, current + templateKey[key].length) ===
+          templateKey[key]
+        ) {
+          tokens.push({
+            type: templateKey[key],
+          });
+          current += templateKey[key].length;
+          isMatchKeyword = true;
+          break;
+        }
+      }
+      if (isMatchKeyword) continue;
+    }
+    if (!tokens.length) {
+      tokens.push({
+        value: char,
+      });
+    } else {
+      const lastToken = tokens[tokens.length - 1];
+      if (lastToken.type === undefined) {
+        lastToken.value += char;
+      } else {
+        tokens.push({
+          value: char,
+        });
+      }
+    }
+    current++;
+  }
+
+  return tokens;
+}
+
+function parse(tokens) {
+  let ast = [];
+  let loopStack = [];
+  let current = 0;
+  while (current < tokens.length) {
+    let token = tokens[current];
+    if (token.type === templateKey.loopStart) {
+      let loopNode = {
+        type: "Loop",
+        values: [],
+      };
+      if (loopStack.length) {
+        let parentLoop = loopStack[loopStack.length - 1];
+        parentLoop.values.push(loopNode);
+      } else {
+        ast.push(loopNode);
+      }
+      loopStack.push(loopNode);
+    } else if (token.type === templateKey.loopEnd) {
+      loopStack.pop();
+    } else {
+      const target = loopStack.length
+        ? loopStack[loopStack.length - 1].values
+        : ast;
+      target.push({
+        type: "Text",
+        value: token.value,
+      });
+    }
+    current++;
+  }
+  return ast;
+}
+
+function evaluate(node, isLoop, data) {
+  if (!node) {
+    return;
+  }
+  let result = "";
+  if (node.type === "Body") {
+    node.values.forEach((value) => {
+      result += evaluate(value, value.type === "Loop", data);
+    });
+  } else if (node.type === "Text") {
+    if (isLoop) {
+      for (const d of data) {
+        result += node.value
+          .replaceAll(templateKey.botName, d.botName)
+          .replaceAll(templateKey.botResponse, d.botResponse);
+      }
+    } else {
+      return node.value;
+    }
+    return result;
+  } else if (node.type === "Loop") {
+    node.values.forEach((value) => {
+      result += evaluate(value, true, data);
+    });
+  }
+  return result;
+}
+
+function preview(template, data) {
+  const tokens = lexer(template);
+  const ast = parse(tokens);
+  return evaluate({ type: "Body", values: ast }, false, data);
+}
+
+export { templateKey, templatePlaceholder, preview };
