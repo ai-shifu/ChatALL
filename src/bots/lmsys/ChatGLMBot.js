6cfe395d7c6ff32801d72feb933b910c131acfd7
Sunner Sun <sunner@gmail.com>
2023-08-13 22:43:13 +0800

    feat: upgrade to LMSYS chatglm2-6b & vicuna-33b

--- src/bots/lmsys/ChatGLMBot.js
+++ src/bots/lmsys/ChatGLMBot.js
@@ -4,7 +4,7 @@ export default class ChatGLMBot extends LMSYSBot {
   static _brandId = "lmsys"; // Brand id of the bot, should be unique. Used in i18n.
   static _className = "ChatGLMBot"; // Class name of the bot
   static _logoFilename = "chatglm-logo.svg"; // Place it in public/bots/
-  static _model = "chatglm-6b";
+  static _model = "chatglm2-6b";
 
   constructor() {
     super();
--- src/bots/lmsys/VicunaBot.js
+++ src/bots/lmsys/VicunaBot.js
@@ -4,7 +4,7 @@ export default class VicunaBot extends LMSYSBot {
   static _brandId = "lmsys"; // Brand id of the bot, should be unique. Used in i18n.
   static _className = "VicunaBot"; // Class name of the bot
   static _logoFilename = "vicuna-logo.jpeg"; // Place it in public/bots/
-  static _model = "vicuna-13b";
+  static _model = "vicuna-33b";
 
   constructor() {
     super();
