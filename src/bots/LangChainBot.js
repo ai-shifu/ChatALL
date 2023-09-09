52102b0842eda3c1a41bc10f9c173c1b02a5569d
panjf@anchnet.com <panjf@anchnet.com>
2023-09-09 18:26:09 +0800

    Optimize by implementing the historical context of LangChainBot using the BufferMemory encapsulated by LangChain.

--- src/bots/LangChainBot.js
+++ src/bots/LangChainBot.js
@@ -7,42 +7,42 @@ export default class LangChainBot extends Bot {
 
   constructor() {
     super();
-    this.bufferMemory = new BufferMemory();
   }
 
   async _sendPrompt(prompt, onUpdateResponse, callbackParam) {
-    // Fetch the chat messages from the buffer memory.
     let messages = await this.getChatContext();
-    // Clear the buffer memory and set the new chat title as the memory key.
-    await this.bufferMemory.clear();
-    messages = messages.map((item) => {
-      let storedMessage = JSON.parse(item); // Deserialize
-      if (
-        storedMessage.type.toLowerCase() ===
-        this.bufferMemory.humanPrefix.toLowerCase()
-      ) {
-        this.bufferMemory.chatHistory.addUserMessage(storedMessage.data);
-      } else if (
-        storedMessage.type.toLowerCase() ===
-        this.bufferMemory.aiPrefix.toLowerCase()
-      ) {
-        this.bufferMemory.chatHistory.addAIChatMessage(storedMessage.data);
-      } else if (storedMessage.type === "system") {
-        this.bufferMemory.chatHistory.addMessage(storedMessage.data);
-      }
-    });
+
+    let bufferMemory = new BufferMemory();
+
     // Remove old messages if exceeding the pastRounds limit
     while (messages.length > this.getPastRounds() * 2) {
       messages.shift();
     }
+
+    messages.forEach((item) => {
+      let storedMessage = JSON.parse(item); // Deserialize
+      if (
+        storedMessage.type.toLowerCase() ===
+        bufferMemory.humanPrefix.toLowerCase()
+      ) {
+        bufferMemory.chatHistory.addUserMessage(storedMessage.data);
+      } else if (
+        storedMessage.type.toLowerCase() === bufferMemory.aiPrefix.toLowerCase()
+      ) {
+        bufferMemory.chatHistory.addAIChatMessage(storedMessage.data);
+      } else if (storedMessage.type === "system") {
+        bufferMemory.chatHistory.addMessage(storedMessage.data);
+      }
+    });
+
     // Deserialize the messages and convert them to the correct format
 
     // Add the prompt to the messages
-    await this.bufferMemory.chatHistory.addUserMessage(prompt);
+    await bufferMemory.chatHistory.addUserMessage(prompt);
 
     let res = "";
     const model = this.constructor._chatModel;
-    messages = await this.bufferMemory.chatHistory.getMessages();
+    messages = await bufferMemory.chatHistory.getMessages();
     const callbacks = [
       {
         handleLLMNewToken(token) {
@@ -56,7 +56,7 @@ export default class LangChainBot extends Bot {
     ];
     model.callbacks = callbacks;
     await model.call(messages);
-    await this.bufferMemory.chatHistory.addAIChatMessage(res);
+    await bufferMemory.chatHistory.addAIChatMessage(res);
     // Serialize the messages before storing
     messages = messages.map((item) => JSON.stringify(item.toDict()));
     this.setChatContext(messages);
