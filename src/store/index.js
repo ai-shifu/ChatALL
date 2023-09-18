5760e761de505a06c1c42d299f2f834b7d994c7d
Sunner Sun <sunner@gmail.com>
2023-09-18 17:19:08 +0800

    feat: add Claude Instant and Falcon 180B as default

--- src/store/index.js
+++ src/store/index.js
@@ -94,6 +94,8 @@ export default createStore({
           { classname: "BingChatBalancedBot", selected: true },
           { classname: "BingChatPreciseBot", selected: true },
           { classname: "BardBot", selected: true },
+          { classname: "ClaudeInstantPoeBot", selected: true },
+          { classname: "Falcon180bBot", selected: true },
           { classname: "Llama270bBot", selected: true },
           { classname: "VicunaBot", selected: true },
         ],
