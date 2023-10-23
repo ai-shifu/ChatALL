a0fca02bbef9bcd9b456efc1b9f2c2af4f79052b
Chek Wei Tan <weiwei1@live.com.my>
2023-10-23 08:04:38 +0800

    Fix migration issue (#604)
    * Add null checking, avoid duplicate key error
    
    * Fix historical thread message not showing up before migration
    
    * Fix historical thread message not showing up after migration

--- src/store/migration.js
+++ src/store/migration.js
@@ -12,6 +12,9 @@ const migrateChatsMessagesThreads = async () => {
   const threadsMigrateData = [];
   try {
     if (localStorage.getItem("isMigratedChatsMessagesThreads") === "true") {
+      if (localStorage.getItem("isMigratedHasThread") !== "true") {
+        migrateHasThread();
+      }
       return;
     }
     const data = JSON.parse(localStorage.getItem("chatall-messages"));
@@ -22,21 +25,39 @@ const migrateChatsMessagesThreads = async () => {
     const chats = data.chats;
     for (let i = 0; i < chats.length; i++) {
       const chat = chats[i];
+      if (!chat) {
+        continue;
+      }
+      chat.index = getIndex(chatIndexMap, chat.index);
       chat.index = getAndGenerateUuidIfNotExist(chatIndexMap, chat.index);
       chat.modifiedTime = chat.createdTime;
       let createdTime = chat.createdTime || 0;
       for (let j = 0; j < chat.messages.length; j++) {
         const message = chat.messages[j];
+        if (!message) {
+          continue;
+        }
+        message.index = getIndex(messageIndexMap, message.index);
         message.index = getAndGenerateUuidIfNotExist(
           messageIndexMap,
           message.index,
         );
         message.chatIndex = chat.index;
         message.createdTime = createdTime + j;
+        if (chat.threads && chat.threads.length) {
+          message.hasThread = true;
+        }
         messagesMigrateData.push(message);
+      }
+      if (!chat.threads) {
+        // skip for old version do not have threads array
+        continue;
       }
       for (let q = 0; q < chat.threads.length; q++) {
         const thread = chat.threads[q];
+        if (!thread) {
+          continue;
+        }
         const messageIndex = getAndGenerateUuidIfNotExist(
           messageIndexMap,
           thread.responseIndex,
@@ -44,6 +65,10 @@ const migrateChatsMessagesThreads = async () => {
         let createdTime = chat.createdTime || 0;
         for (let p = 0; p < thread.messages.length; p++) {
           const threadMessage = thread.messages[p];
+          if (!threadMessage) {
+            continue;
+          }
+          threadMessage.index = getIndex(threadIndexMap, threadMessage.index);
           threadMessage.index = getAndGenerateUuidIfNotExist(
             threadIndexMap,
             threadMessage.index,
@@ -84,6 +109,7 @@ const migrateChatsMessagesThreads = async () => {
       console.error("Threads Migration error:", error);
     }
     localStorage.setItem("isMigratedChatsMessagesThreads", true);
+    localStorage.setItem("isMigratedHasThread", true);
     console.log("Migration done");
   } catch (error) {
     console.error("Migration error:", error);
@@ -95,6 +121,30 @@ function getAndGenerateUuidIfNotExist(map, key) {
     map[key] = uuidv4();
   }
   return map[key];
+}
+
+function getIndex(map, key) {
+  if (map[key] !== undefined) {
+    // duplicate key not allowed
+    return uuidv4();
+  }
+  return key;
+}
+
+async function migrateHasThread() {
+  // update Message hasThread property to `true` if messageIndex existed in Thread
+  const messageIndexes = await Threads.table
+    .orderBy("messageIndex")
+    .distinct()
+    .uniqueKeys();
+  for (const index of messageIndexes) {
+    try {
+      await Messages.table.update(index, { hasThread: true });
+    } catch (error) {
+      console.error("migrateHasThread error:", error);
+    }
+  }
+  localStorage.setItem("isMigratedHasThread", true);
 }
 
 export { migrateChatsMessagesThreads };
