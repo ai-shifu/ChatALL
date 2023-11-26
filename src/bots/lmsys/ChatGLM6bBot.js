fa405dffb5f993b2d3cb5100940042cb1882dd45
yaoyilin <yaoyilin@aliyun.com>
2023-11-26 23:31:35 +0800

    update chatglm logo js

--- src/bots/lmsys/ChatGLM6bBot.js
+++ src/bots/lmsys/ChatGLM6bBot.js
@@ -3,7 +3,7 @@ import LMSYSBot from "./LMSYSBot";
 export default class ChatGLM6bBot extends LMSYSBot {
   static _brandId = "lmsys"; // Brand id of the bot, should be unique. Used in i18n.
   static _className = "ChatGLM6bBot"; // Class name of the bot
-  static _logoFilename = "chatglm-logo.svg"; // Place it in public/bots/
+  static _logoFilename = "chatglm2-logo.png"; // Place it in public/bots/
   static _model = "chatglm2-6b";
 
   constructor() {
