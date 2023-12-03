813d13c408e10b449cc7fc4e45f1d875ee073b4b
Sunner Sun <sunner@gmail.com>
2023-12-03 21:09:55 +0800

    add new logo image

--- src/bots/openai/OpenAIAPI4128KBot.js
+++ src/bots/openai/OpenAIAPI4128KBot.js
@@ -2,7 +2,7 @@ import OpenAIAPIBot from "./OpenAIAPIBot";
 
 export default class OpenAIAPI4128KBot extends OpenAIAPIBot {
   static _className = "OpenAIAPI4128KBot"; // Class name of the bot
-  static _logoFilename = "openai-4-logo.png"; // Place it in public/bots/
+  static _logoFilename = "openai-4-128k-logo.png"; // Place it in public/bots/
   static _isDarkLogo = true; // The main color of logo is dark
   static _model = "gpt-4-1106-preview";
 
