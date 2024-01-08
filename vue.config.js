d326e46403e0a3c5318c6a1dcbb122a79e69e0d1
Awesdroid <awesdroid@gmail.com>
2024-01-09 06:17:21 +0800

    Add configureWebpack for debug

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  configureWebpack: {
+    devtool: "source-map",
+  },
   transpileDependencies: ["vuetify"],
   pluginOptions: {
     electronBuilder: {
