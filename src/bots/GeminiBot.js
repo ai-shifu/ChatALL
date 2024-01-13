7ee941ea54fe247d14c6c1b0b2668e599ca9bf7c
awesdroid <awesdroid@users.noreply.github.com>
2024-01-13 12:33:52 +0800

    Add Gemini-pro model for chat (#678)
    * Add langchain/google-genai package and update langchain to 0.0.207 for Gemini
    
    * Gemini up and running
    
    * Update gemini-logo.png
    
    ---------
    
    Co-authored-by: Sunner Sun <sunner@gmail.com>

---
+++ src/bots/GeminiBot.js
@@ -0,0 +1,36 @@
+import LangChainBot from "@/bots/LangChainBot";
+import store from "@/store";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+export default class GeminiBot extends LangChainBot {
+  static _brandId = "gemini";
+  static _className = "GeminiBot";
+  static _logoFilename = "gemini-logo.png"; // Place it in public/bots/
+  static _model = "gemini-pro";
+
+  constructor() {
+    super();
+  }
+
+  async _checkAvailability() {
+    let available = false;
+
+    if (store.state.gemini.apiKey) {
+      const chatModel = new ChatGoogleGenerativeAI({
+        apiKey: store.state.gemini.apiKey,
+        modelName: this.constructor._model ? this.constructor._model : "",
+        temperature: store.state.gemini.temperature,
+        streaming: true,
+        topK: store.state.gemini.topK,
+        topP: store.state.gemini.topP,
+      });
+      this.constructor._chatModel = chatModel;
+      available = true;
+    }
+    return available;
+  }
+
+  getPastRounds() {
+    return store.state.gemini.pastRounds;
+  }
+}
--- src/bots/index.js
+++ src/bots/index.js
@@ -1,4 +1,5 @@
 // Bots
+import GeminiBot from "@/bots/GeminiBot";
 import ChatGPT35Bot from "@/bots/openai/ChatGPT35Bot";
 import ChatGPT4Bot from "@/bots/openai/ChatGPT4Bot";
 import ChatGPTBrowsingBot from "@/bots/openai/ChatGPTBrowsingBot";
@@ -60,6 +61,7 @@ import WenxinQianfan4Bot from "./baidu/WenxinQianfan4Bot";
 import Zephyr7bBot from "./huggingface/Zephyr7bBot";
 
 const all = [
+  GeminiBot.getInstance(),
   Qihoo360AIBrainBot.getInstance(),
   AlpacaBot.getInstance(),
   SageBot.getInstance(), // Assistant@Poe
@@ -217,6 +219,7 @@ export const botTags = {
     bots.getBotByClassName("Zephyr7bBot"),
   ],
   api: [
+    bots.getBotByClassName("GeminiBot"),
     bots.getBotByClassName("AzureOpenAIAPIBot"),
     bots.getBotByClassName("OpenAIAPI35Bot"),
     bots.getBotByClassName("OpenAIAPI3516KBot"),
