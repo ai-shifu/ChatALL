5c8931e54e1f2db190575484103f07dfea96063b
Peter Dave Hello <hsu@peterdavehello.org>
2024-04-15 00:38:32 +0800

    Move GPT-4 Turbo API to the latest GA version
    Reference:
    - https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo

--- src/bots/openai/OpenAIAPI4128KBot.js
+++ src/bots/openai/OpenAIAPI4128KBot.js
@@ -4,7 +4,7 @@ export default class OpenAIAPI4128KBot extends OpenAIAPIBot {
   static _className = "OpenAIAPI4128KBot"; // Class name of the bot
   static _logoFilename = "openai-4-128k-logo.png"; // Place it in public/bots/
   static _isDarkLogo = true; // The main color of logo is dark
-  static _model = "gpt-4-turbo-preview";
+  static _model = "gpt-4-turbo";
 
   constructor() {
     super();
