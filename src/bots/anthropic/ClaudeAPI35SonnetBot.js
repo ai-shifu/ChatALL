45a76d3313857ec87887a7629015afbfff409b98
Peter Dave Hello <hsu@peterdavehello.org>
2024-06-21 01:15:35 +0800

    Add Anthropic Claude 3.5 Sonnet via API
    Reference:
    - https://www.anthropic.com/news/claude-3-5-sonnet
    - https://docs.anthropic.com/en/docs/about-claude/models#model-names

---
+++ src/bots/anthropic/ClaudeAPI35SonnetBot.js
@@ -0,0 +1,10 @@
+import ClaudeAPIBot from "./ClaudeAPIBot";
+
+export default class ClaudeAPI35SonnetBot extends ClaudeAPIBot {
+  static _className = "ClaudeAPI35SonnetBot";
+  static _logoFilename = "claudeapi-sonnet-logo.png";
+  static _model = "claude-3-5-sonnet-20240620";
+  constructor() {
+    super();
+  }
+}
