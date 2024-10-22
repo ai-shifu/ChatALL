21cac45950310db3df60dfe637f87215cc4a3d84
Peter Dave Hello <hsu@peterdavehello.org>
2024-10-23 01:42:12 +0800

    Update Claude 3.5 Sonnet to claude-3-5-sonnet-latest
    Reference:
    - https://docs.anthropic.com/en/docs/about-claude/models#model-names

--- src/bots/anthropic/ClaudeAPI35SonnetBot.js
+++ src/bots/anthropic/ClaudeAPI35SonnetBot.js
@@ -3,7 +3,7 @@ import ClaudeAPIBot from "./ClaudeAPIBot";
 export default class ClaudeAPI35SonnetBot extends ClaudeAPIBot {
   static _className = "ClaudeAPI35SonnetBot";
   static _logoFilename = "claudeapi-sonnet-logo.png";
-  static _model = "claude-3-5-sonnet-20240620";
+  static _model = "claude-3-5-sonnet-latest";
   constructor() {
     super();
   }
