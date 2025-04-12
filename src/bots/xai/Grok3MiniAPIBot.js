0976dafc49da7bc94194516f3f5e1f2e7a302b35
Peter Dave Hello <hsu@peterdavehello.org>
2025-04-13 00:40:51 +0800

    Add xAI Grok 3 mini via official API, cc #977 (#999)

---
+++ src/bots/xai/Grok3MiniAPIBot.js
@@ -0,0 +1,10 @@
+import xAIAPIBot from "./xAIAPIBot";
+
+export default class Grok3MiniAPIBot extends xAIAPIBot {
+  static _className = "Grok3MiniAPIBot";
+  static _logoFilename = "grok-3-mini-logo.png";
+  static _model = "grok-3-mini-beta";
+  constructor() {
+    super();
+  }
+}
