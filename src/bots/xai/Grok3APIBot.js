b5e43b29674b22535bb7f95d8c5b4c530349eff5
Peter Dave Hello <hsu@peterdavehello.org>
2025-05-20 21:35:26 -0700

    Update Grok 3 series model, remove `-beta` suffix (#1016)

--- src/bots/xai/Grok3APIBot.js
+++ src/bots/xai/Grok3APIBot.js
@@ -3,7 +3,7 @@ import xAIAPIBot from "./xAIAPIBot";
 export default class Grok3APIBot extends xAIAPIBot {
   static _className = "Grok3APIBot";
   static _logoFilename = "grok-3-logo.png";
-  static _model = "grok-3-beta";
+  static _model = "grok-3";
   constructor() {
     super();
   }
--- src/bots/xai/Grok3MiniAPIBot.js
+++ src/bots/xai/Grok3MiniAPIBot.js
@@ -3,7 +3,7 @@ import xAIAPIBot from "./xAIAPIBot";
 export default class Grok3MiniAPIBot extends xAIAPIBot {
   static _className = "Grok3MiniAPIBot";
   static _logoFilename = "grok-3-mini-logo.png";
-  static _model = "grok-3-mini-beta";
+  static _model = "grok-3-mini";
   constructor() {
     super();
   }
